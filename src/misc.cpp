86525e780cc0174a81f8986401fd7ad908e43461
u15119 <u15119@23e97676-e3e9-41ff-bed1-4a4288a26c68>
2012-04-11 06:31:15 +0000

    ;)
    git-svn-id: svn://svn.code.sf.net/p/cdcat/svn/trunk@444 23e97676-e3e9-41ff-bed1-4a4288a26c68

--- src/misc.cpp
+++ src/misc.cpp
@@ -234,10 +234,10 @@ QString getCDSerial ( const char *CDpath ) {
 	if ( !GetVolumeInformationA ( CDpath, bufName1, 64, &dwSerial, NULL, NULL, NULL, 0 ) ) {
 		return QString ( "" );
 	}
-	char *myChar = "Hello World";
+// 	char *myChar = "Hello World";
 	//DWORD dw = reinterpret_cast<DWORD> ( myChar );
 	//bufName1 = reinterpret_cast<char*> ( dw );
-	bufName1 = strncpy(&bufName1, myChar, 63);
+// 	bufName1 = strncpy(&bufName1, myChar, 63);
 	//name = QString ( bufName1 ).toLocal8Bit().data();
 	//QMessageBox::information(0, "vol1", "path: "+QString(CDpath)+"\nname: \""+QString(name)+"\"");
 	return QString ( bufName1 );
