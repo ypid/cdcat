f58dce6d12d5501cac0ad51005b37be27cf24aa2
Robin Schneider <ypid@riseup.net>
2015-04-24 20:23:14 +0200

    Optimized script.

--- src/build_translations_mac.sh
+++ src/build_translations_mac.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-files=$(find . -type f -name '*.ts')
-for f in $files; do
+
+for f in $(find . -type f -name '*.ts'); do
     f2=$(echo $f |sed -e 's/ts$/qm/g')
     echo "$0: $f to $f2"
-    lrelease -silent -qm $f2 $f
-    cp $f2 ./cdcat.app/Contents/MacOS/lang/
+    lrelease -silent -qm "$f2" "$f"
+    cp "$f2" ./cdcat.app/Contents/MacOS/lang/
 done
